cacd078210f8158c9148cb8784fffdc4a169d5cc
Richor <richor.gm@gmail.com>
2017-06-06 19:11:24 +0800

    ver 0.0.1

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready (function() {
+	$("a").attr("target", "_blank");
+	// logo 晃动动画
+	// $(".title").mouseover(function() {
+	// 	$(".title").addClass("swing");
+	// });
+	// 悬浮，叶子上浮出现
+	// $(".nav-list li").mouseenter(function() {
+	// 	$(this).prepend("<img src='img/leaf.svg'");
+	// });
+});
